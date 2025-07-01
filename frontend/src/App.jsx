0329e96b7af690059c65faedefad6a30747528dd
Ansar Sultan <ansarsultan67@gmail.com>
2025-07-02 00:23:34 +0500

    added the special menu and top doctors section in the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-profile" element={<MyProfile />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
-        <Route path="/Appointment/:docID" element={<Appointments />} />
+        <Route path="/appointment/:docID" element={<Appointments />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
 
 
 export default function Home(){
     return(
         <div>
             <Header/>
-            Home page
+           <SpecialityMenu/>
+           <TopDoctors/>
         </div>
     )
 }
