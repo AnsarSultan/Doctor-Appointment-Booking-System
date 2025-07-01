a585e40dfa1ddbb92548f2782f5223860b89a466
Ansar Sultan <ansarsultan67@gmail.com>
2025-07-01 22:55:42 +0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors.jsx";
+import Login from "./pages/Login.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import MyProfile from "./pages/MyProfile.jsx";
+import MyAppointments from "./pages/MyAppointments.jsx";
+import Appointments from "./pages/Appointments.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/Appointment/:docID" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Doctors = () => {
+  return (
+    <div>This is doctor page</div>
+  )
+}
+
+export default Doctors
