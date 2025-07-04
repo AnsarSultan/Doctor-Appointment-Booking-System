01779fe2aa36637e5b0837c3fb066b58cafc599a
Ansar Sultan <ansarsultan67@gmail.com>
2025-07-05 01:34:59 +0500

    Created controler and model for doctor and setup the add doctor API

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,54 @@
+import validator from "validator"
+import bycrypt from 'bcrypt'
+import {v2 as cloudinary} from "cloudinary"
+import doctorModel from "../models/doctorModels.js"
+// api for adding doctor
+const addDoctor = async (req , res)=>{
+    try {
+
+        const {name , email , password , speciality , degree , experience , about , fees , address } = req.body
+        const imageFile = req.file
+        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address ){
+            return res.json({success:false, message: "Missing Details"})
+        }
+        if(!validator.isEmail(email)){
+            return res.json({success:false , message: "Please enter a valid email"})
+        }
+
+        if(password.length < 8){
+            return res.json({success:false , message: "Please enter a strong password"})
+        }
+
+        const salt = await bycrypt.genSalt(10)
+        const hashedPassword = await bycrypt.hash(password , salt)
+
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"}) 
+        const imageUrl = imageUpload.secure_url
+
+        const doctorData = {
+            name,
+            email,
+            image: imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        await newDoctor.save()
+        res.json({success:true , message: "Doctor added"})
+
+
+        res.send("data stored successfully")
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message: error.message})
+    }
+}
+
+export {addDoctor}
