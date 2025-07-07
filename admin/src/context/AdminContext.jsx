fffcc34030b87278f66a3915fcf029db5a26254e
Ansar Sultan <ansarsultan67@gmail.com>
2025-07-08 01:15:21 +0500

    setup routesfor admin panel and make the frontend of add doctor page

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,10 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props)=>{
+    const [aToken,setAToken] = useState(localStorage.getItem('aToken') || '')
+    const backendURL = import.meta.env.VITE_BACKEND_URL
     const value = {
-
+        aToken , setAToken,
+        backendURL,
     }
     return (
         <AdminContext.Provider value={value}>
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -57,7 +57,7 @@ const loginAdmin = async (req, res)=>{
         const {email , password} = req.body
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
             const token = jwt.sign(email+password, process.env.JWT_SECRET);
-            res.json({success:true , token})
+            res.json({success:true , message: "Login successfully" , token})
         }else{
             res.json({success: false , message: "Invalid credential"})
         }
