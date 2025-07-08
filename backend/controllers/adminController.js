21e232a0681fd1095f6e70eac8bd318b2ffcefba
Ansar Sultan <ansarsultan67@gmail.com>
2025-07-09 01:22:37 +0500

    fetched all doctors from the DB and displa yit in the admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -66,4 +66,18 @@ const loginAdmin = async (req, res)=>{
         res.json({success:false, message: error.message})
     }
 }
-export {addDoctor , loginAdmin}
+
+
+// api to get all dcotor list for admin panel
+
+const allDoctors = async (req, res)=>{
+    try {
+        const doctors = await doctorModel.find({}).select('-password')
+        res.json({success:true , doctors})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message:error.message})
+    }
+
+}
+export {addDoctor , loginAdmin , allDoctors}
